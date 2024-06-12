a4f7169920eadbc4fedbe1b265716c818feae1f5
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-06-12 15:10:12 +0100

    Removed global variables & unneeded assignments
    Moved to global variables for the players choice and computers choice
    to the local scope of their respective functions
    Removed unneeded variable assignments as they aren't needed to return
    correct value

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -5,32 +5,26 @@
 console.log("Hello World!");
 
 //Variables
-let computerChoice = 0;
-let humanChoice = "";
 
-//testing!! Call functions to make sure they work
-getComputerChoice();
-getHumanChoice();
-
-//check variables in console
-console.log("Computers Choice Is " + computerChoice);
-console.log("Human Choice Is " + humanChoice);
+//testing!! Call functions and log to console
+console.log("Computers Choice Is " + getComputerChoice());
+console.log("Human Choice Is " + getHumanChoice());
 
 //function to get the computers choice of rock paper or scissors
 function getComputerChoice() {
     //generate random number between 1 & 3
     //store in computerChoice variable
-    computerChoice = Math.floor(Math.random() * 3) + 1;
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
     console.log(computerChoice);
     //check if if random number is 1, 2 or 3 then return appropriate answer
     if (computerChoice == 1) {
-        return computerChoice = "Rock";
+        return "Rock";
 
     } else if(computerChoice == 2) {
-        return computerChoice = "Paper";
+        return "Paper";
 
     } else {
-        return computerChoice = "Scissors";
+        return "Scissors";
     }
 
 }
@@ -38,17 +32,17 @@ function getComputerChoice() {
 //function to get the human players choice
 function getHumanChoice() {
     //ask player for their Choice and store in humanChoice
-    humanChoice = prompt("Type Rock, Paper Or Scissors below to make your choice");
+    let humanChoice = prompt("Type Rock, Paper Or Scissors below to make your choice");
     console.log(humanChoice);
     //make human choice all lower case and return
     if(humanChoice.toLowerCase() === "rock") {
-        return humanChoice = "Rock";
+        return "Rock";
 
     } else if(humanChoice.toLowerCase() === "paper") {
-        return humanChoice = "Paper";
+        return "Paper";
 
     } else if(humanChoice.toLowerCase() === "scissors") {
-        return humanChoice = "Scissors"
+        return "Scissors"
 
     }else {
         alert("please only enter either Rock Paper or Scissors")
