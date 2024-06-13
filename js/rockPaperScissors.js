e39ce72676944c69651d2d043824bb7cf25ebb9b
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-06-13 16:04:48 +0100

    ADD game loop function MOVE round function
    Added function to loop through the round function 5 times and moved
    playRound function and global variables for score and round count
    inside
    added a display score function with logic to figure out who won
    and display an appropriate message

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -2,16 +2,70 @@
 //create the game Rock Paper Scissors. 
 //This game will be played entirely in the console.
 //just saying hello!!
-console.log("Hello World!");
+//console.log("Hello World!");
 
-//Variables
-let humanScore = 0,
-    computerScore = 0;
+//play the game
+playGame();
 
-//testing!! Call functions and log to console
-//console.log("Computers Choice Is " + getComputerChoice());
-//console.log("Human Choice Is " + getHumanChoice());
-playRound(getHumanChoice(), getComputerChoice());
+function playGame() {
+    //Variables
+    let humanScore = 0,
+        computerScore = 0,
+        roundNumber = 0;
+    //loop until 5 rounds have been achieved and print out a winner
+    for(roundNumber = 1; roundNumber < 6; roundNumber++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    //function to play one round of the game
+    function playRound(humanChoice, computerChoice) {
+        //rock >> scissors >> paper >> rock
+        //check to see if its a draw before doing anything else
+        console.log("human " + humanChoice);
+        console.log("computer " + computerChoice);
+        if(humanChoice === computerChoice) {
+            console.log("You Both Picked " + computerChoice + "! it's a Draw!!!");
+
+        } else if((humanChoice === "Rock" && computerChoice === "Scissors") 
+                || (humanChoice === "Scissors" && computerChoice === "Paper") 
+                || (humanChoice === "Paper" && computerChoice === "Rock")) {
+            //check computer score and print message
+            console.log("You Win!! This Round!! " + humanChoice + " Beats " + computerChoice);
+            //increment the score
+            humanScore +=1;
+        }else {
+            console.log("You Lose!! This Round!! " + computerChoice + " Beats " + humanChoice);
+            //increment the score
+            computerScore +=1;
+        }
+
+        //display scores
+        displayScore(roundNumber, humanScore, computerScore);
+    }
+
+}
+
+//function to display score and winning messages
+function displayScore(roundNumber, humanScore, computerScore) {
+    //print out current score or final score if its the last round
+    console.log(roundNumber);
+    if(roundNumber < 5) {
+        console.log("The Score is currently Player " + humanScore + " Computer " + computerScore);
+    
+    }else {
+        console.log("The Final Score is Player " + humanScore + " Computer " + computerScore);
+         //if you win print congrats if not poor you!!
+        if(humanScore == computerScore) {
+            console.log("Oh No!!  It's A Draw!!");
+    
+        }else if(humanScore > computerScore) {
+            console.log("Congratulations!! You Won!!!");
+    
+        }else {
+            console.log("Oh No!! You Lost!!");
+        }
+    }
+}
 
 //function to get the computers choice of rock paper or scissors
 function getComputerChoice() {
@@ -52,31 +106,4 @@ function getHumanChoice() {
     }
 
 }
-
-//function to play one round of the game
-function playRound(humanChoice, computerChoice) {
-    //rock >> scissors >> paper >> rock
-    //check to see if its a draw before doing anything else
-    console.log("human " + humanChoice);
-    console.log("computer " + computerChoice);
-    if(humanChoice === computerChoice) {
-        console.log("You Both Picked " + computerChoice + "! it's a Draw!!!");
-
-    } else if((humanChoice === "Rock" && computerChoice === "Scissors") 
-            || (humanChoice === "Scissors" && computerChoice === "Paper") 
-            || (humanChoice === "Paper" && computerChoice === "Rock")) {
-        //check computer score and print message
-        console.log("You Win!! " + humanChoice + " Beats " + computerChoice);
-        //increment the score
-        humanScore +=1;
-    }else {
-        console.log("You Lose!! " + computerChoice + " Beats " + humanChoice);
-        //increment the score
-        computerScore +=1;
-    }
-
-    console.log("The Score is currently Human " + humanScore + " Computer " + computerScore);
-}
-
-
 
